c1f8c18d18cc5252e818d5bc0c205278d3268287
Abi Woo-Sam <abixcastro@gmail.com>
2024-01-05 17:23:23 -0800

    add test a solidity event tutorial files

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# test-an-event-with-foundry
-How to test a solidity event within a Hedera smart contract using Foundry.
+# How to Test a Solidity Event with Foundry 
+
+Foundry supports the use of cheatcodes. Cheatcodes allow you to go beyond testing the outputs of your smart contracts. They allow developers to manipulate the state of the blockchain, test for reverts, 
+and events. In this tutorial, we will focus on the cheatcode `vm.expectEmit` to test a solidity event.
+
+## What you will accomplish
+
+- ✅ Use the vm.expectEmit Cheatcode
+- ✅ Test a Solidity event
+
+
+---
+
+## Get started
+
+The repo, github.com/hedera-dev/test-an-event-with-foundry, is intended to be used alongside this [tutorial](docs.hedera.com).
---
+++ test/TodoList.t.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "src/TodoList.sol";
+
+contract TodoListTest is Test {
+  TodoList public todoList;
+  // Step (1) - Define the exepcted event
+   /* ... */
+
+      function setUp() public {
+        todoList = new TodoList();
+    }  
+
+   function test_createTodo_returnsNumberOfTodosIncrementedByOne() public {
+
+     uint256 numberOfTodosBefore = todoList.getNumberOfTodos();
+
+     uint256 numberOfTodosAfter = todoList.createTodo(address(this), "A new todo for you!");
+
+     assertEq(numberOfTodosAfter, (numberOfTodosBefore + 1), "create todo test");
+   }
+
+   function test_emit_createTodoEvent() public {
+    // Step (2) - Get the current number of todos
+    /* ... */
+    
+    // Step (3) - Specify the event data to test
+    /* ... */
+
+    // Step (4) - emit the expected event
+    /* ... */
+
+    // Step (5) - execute the contract function that emits the event
+    /* ... */
+   }
+}
